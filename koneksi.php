1e1258b99d616161733b8d6bb4f3c75f6edfa8f7
mashhisyam <haryoh48@gmail.com>
2021-10-07 11:00:21 +0700

    First Commit

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "cv_db";
+$db_host = "10.0.0.219";
+$db_user = "admin";
+$db_pass = "Hisyam123";
+$db_name = "Tokol";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
