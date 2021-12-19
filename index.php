6a12eb3186f31a3c27d12ce0e6be98d5c8a8303b
mashhisyam <haryoh48@gmail.com>
2021-12-19 18:34:46 +0700

    ubah koneksi index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?//php require_once("koneksi.php");
-    //if (!isset($_SESSION)) {
-    //    session_start();
-    //} //?>
+<?php
+$db_host = "10.0.0.219";
+$db_user = "admin";
+$db_pass = "Hisyam123;";
+$db_name = "Tokol";
+
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(mysqli_connect_error()){
+	echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
